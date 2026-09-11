a9d7f5a342cb6383b8ba94f7e176927218b8361c
asvinthsathiyadhas-cmyk <asvinthsathiyadhas@gmail.com>
2026-09-11 12:39:32 +0530

    dbcontext modified

--- ApartmentInfrastructure/Data/ApartmentDbContext.cs
+++ ApartmentInfrastructure/Data/ApartmentDbContext.cs
@@ -1,175 +1,31 @@
 ﻿using ApartmentDomain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ApartmentInfrastructure.Data;
-
-public class ApartmentDbContext : DbContext
+namespace ApartmentInfrastructure.Data
 {
-    public ApartmentDbContext(
-        DbContextOptions<ApartmentDbContext> options)
-        : base(options)
+    public class ApartmentDbcontext : DbContext
     {
-    }
-
-    public DbSet<User> Users { get; set; }
-        public DbSet<Flat> Flats { get; set; }
-
-    public DbSet<Building> Buildings { get; set; }
-        public DbSet<Maintenance> Maintenances { get; set; }
-
-    public DbSet<Notice> Notices { get; set; }
-        public DbSet<Parking> Parkings { get; set; }
-
-    public DbSet<Document> Documents { get; set; }
-        public DbSet<Staff> Staffs { get; set; }
-
-    protected override void OnModelCreating(
-        ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<User>(entity =>
+        public ApartmentDbcontext(DbContextOptions<ApartmentDbcontext> options)
+            : base(options)
         {
-            entity.HasKey(x => x.Id);
+        }
 
-            entity.Property(x => x.Name)
-                .IsRequired()
-                .HasMaxLength(100);
+        public DbSet<ResidentEntity> Residents { get; set; }
 
-            entity.Property(x => x.Email)
-                .IsRequired()
-                .HasMaxLength(150);
+        public DbSet<PaymentEntity> Payments { get; set; }
 
-            entity.HasIndex(x => x.Email)
-                .IsUnique();
+        public DbSet<ComplaintEntity> Complaints { get; set; }
 
-            entity.Property(x => x.Password)
-                .IsRequired();
+        public DbSet<EmergencyEntity> Emergencies { get; set; }
 
-            entity.Property(x => x.PhoneNumber)
-                .HasMaxLength(20);
-        });
-
-        modelBuilder.Entity<Building>(entity =>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            entity.HasKey(x => x.Id);
+            base.OnModelCreating(modelBuilder);
 
-            entity.Property(x => x.Name)
-                .IsRequired()
-                .HasMaxLength(150);
-
-            entity.Property(x => x.Address)
-                .IsRequired()
-                .HasMaxLength(250);
-        });
-
-        modelBuilder.Entity<Notice>(entity =>
-        {
-            entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.Title)
-                .IsRequired()
-                .HasMaxLength(200);
-
-            entity.Property(x => x.Description)
-                .IsRequired();
-        });
-
-        modelBuilder.Entity<Document>(entity =>
-        {
-            entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.Title)
-                .IsRequired()
-                .HasMaxLength(200);
-
-            entity.Property(x => x.FileName)
-                .IsRequired()
-                .HasMaxLength(255);
-
-            entity.Property(x => x.FilePath)
-                .IsRequired()
-                .HasMaxLength(500);
-        });
-
-        // Flat configuration
-        modelBuilder.Entity<Flat>(entity =>
-        {
-            entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.FlatNumber)
-                  .IsRequired()
-                  .HasMaxLength(20);
-
-            entity.Property(x => x.FlatType)
-                  .IsRequired()
-                  .HasMaxLength(20);
-
-            entity.Property(x => x.Status)
-                  .IsRequired();
-        });
-
-        // Maintenance configuration
-        modelBuilder.Entity<Maintenance>(entity =>
-        {
-            entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.Amount)
-                  .HasPrecision(18, 2);
-
-            entity.Property(x => x.Status)
-                  .IsRequired();
-
-            entity.HasOne<Flat>()
-                  .WithMany()
-                  .HasForeignKey(x => x.FlatId)
-                  .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        // Parking configuration
-        modelBuilder.Entity<Parking>(entity =>
-        {
-            entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.SlotNumber)
-                  .IsRequired()
-                  .HasMaxLength(20);
-
-            entity.Property(x => x.VehicleNumber)
-                  .HasMaxLength(20);
-
-            entity.Property(x => x.Status)
-                  .IsRequired();
-
-            entity.HasOne<Flat>()
-                  .WithMany()
-                  .HasForeignKey(x => x.FlatId)
-                  .OnDelete(DeleteBehavior.SetNull);
-        });
-
-        // Staff configuration
-        modelBuilder.Entity<Staff>(entity =>
-        {
-            entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.Name)
-                  .IsRequired()
-                  .HasMaxLength(100);
-
-            entity.Property(x => x.Phone)
-                  .IsRequired()
-                  .HasMaxLength(15);
-
-            entity.Property(x => x.JobRole)
-                  .IsRequired()
-                  .HasMaxLength(50);
-
-            entity.Property(x => x.Status)
-                  .IsRequired();
-        });
+            // Payment amount precision
+            modelBuilder.Entity<PaymentEntity>()
+                .Property(p => p.Amount)
+                .HasPrecision(18, 2);
+        }
     }
 }
-
